c8d5f76a31098890a8f08ce3a8aca30d55f68433
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2022-09-11 13:54:11 +0200

    Add function playGround

--- main.js
+++ main.js
@@ -3,12 +3,28 @@
     The desired outcome is that the computer randomly 1 of the 3
     choices each game/run.  */
 
-    function getComputerChoice() {
-        
+    // This function randomly pick 1 of the 3 values //
+    function getComputerChoice() {   
+
         let value = ['Rock', 'Paper', 'Scissors'];
             let randomValue = value[Math.floor(Math.random() * value.length)];
 
-            console.log(randomValue);
+            return (randomValue);
     }
 
-    console.log(getComputerChoice());
+    function playRound(playerSelection, computerSelection) {
+
+        if (computerSelection === 'Scissors') {
+            return('You won! Rock beats scissors');
+        } 
+        else if (computerSelection === 'Paper') {
+            return("You lost...Rock loses to paper");
+        }
+        else {
+            return("It's a draw!");
+        }
+    }
+
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
